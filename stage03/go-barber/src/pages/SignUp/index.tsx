bd26c9a410ea22ef832f1442a6ed072b6cf0cd3e
SkyHeads <skyheads@live.com>
2020-04-26 17:09:26 -0300

    add yup and schema validator signup

--- stage03/go-barber/src/pages/SignUp/index.tsx
+++ stage03/go-barber/src/pages/SignUp/index.tsx
@@ -2,6 +2,8 @@ import React, { useCallback } from 'react';
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
+import * as Yup from 'yup';
+
 import { Form } from '@unform/web';
 
 import logo from '../../assets/logo.svg';
@@ -12,8 +14,24 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  const handleSubmit = useCallback((event: string) => {
-    console.log(event);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string()
+          .min(6, 'No mínimo 6 dígitos')
+          .required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
