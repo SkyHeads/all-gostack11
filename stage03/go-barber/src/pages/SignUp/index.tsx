f9627342d4ff60a30a9ccf8e0d05f80c207a35c3
SkyHeads <skyheads@live.com>
2020-04-26 01:56:28 -0300

    input styles and usecallback

---
+++ stage03/go-barber/src/pages/SignUp/index.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from 'react';
+
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback((event: string) => {
+    console.log(event);
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="Go Barber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="forgot">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
