542c2f8285fe2bc8daf731c3163ca9d4098f67db
SkyHeads <skyheads@live.com>
2020-05-01 00:01:24 -0300

    add tooltip signup

--- stage03/go-barber/src/components/Tooltip/index.tsx
+++ stage03/go-barber/src/components/Tooltip/index.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Tooltip: React.FC = () => <h1>Tooltip</h1>;
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  title,
+  className = '',
+  children,
+}) => {
+  return (
+    <Container className={className}>
+      {children}
+      <span>{title}</span>
+    </Container>
+  );
+};
 
 export default Tooltip;
