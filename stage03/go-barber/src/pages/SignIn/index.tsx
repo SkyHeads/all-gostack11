ebd60b2fef8ebadf39cf8d41172338c8d133c974
SkyHeads <skyheads@live.com>
2020-05-02 01:34:51 -0300

    add appprovider/app and call addtoast signin

--- stage03/go-barber/src/pages/SignIn/index.tsx
+++ stage03/go-barber/src/pages/SignIn/index.tsx
@@ -6,7 +6,9 @@ import * as Yup from 'yup';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 
-import { useAuth } from '../../hooks/Auth';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logo from '../../assets/logo.svg';
@@ -25,6 +27,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -42,7 +45,7 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -52,9 +55,10 @@ const SignIn: React.FC = () => {
 
           formRef.current?.setErrors(errors);
         }
+        addToast();
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
