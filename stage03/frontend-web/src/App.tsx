03778aac1ca049534361c9a54a99523a2aa68ff4
Bruno Fernandes <skyheads@live.com>
2020-05-16 19:03:45 -0300

    initial signin page

--- stage03/frontend-web/src/App.tsx
+++ stage03/frontend-web/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  return <GlobalStyle />;
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
