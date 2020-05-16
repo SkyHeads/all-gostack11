f5cd0d41ea3d7f45070c58df42c21710bb43f9dd
Bruno Fernandes <skyheads@live.com>
2020-05-16 19:29:52 -0300

    signin content style

--- stage03/frontend-web/src/pages/SignIn/styles.ts
+++ stage03/frontend-web/src/pages/SignIn/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { shade } from 'polished';
+
 export const Container = styled.div`
   height: 100vh;
 
@@ -7,6 +9,37 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+`;
 
 export const Background = styled.div``;
