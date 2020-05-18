b55aab97defcdaca1217c3e508b6f95d829c3dce
Bruno Fernandes <skyheads@live.com>
2020-05-18 16:40:59 -0300

    formRef current setErrors/show errors in input

--- stage03/frontend-web/src/pages/SignUp/index.tsx
+++ stage03/frontend-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
-
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
@@ -12,6 +12,8 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
       const schema = Yup.object().shape({
@@ -26,7 +28,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      formRef.current?.setErrors({
+        name: 'Nome obrigatório',
+      });
     }
   }, []);
 
@@ -36,7 +40,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" placeholder="Name" icon={FiUser} />
