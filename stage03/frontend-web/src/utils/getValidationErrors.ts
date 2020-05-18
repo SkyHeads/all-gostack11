0e018891345403c50f4419631f9d55b197d6844a
Bruno Fernandes <skyheads@live.com>
2020-05-18 16:47:17 -0300

    end utils getvalidationerrors

--- stage03/frontend-web/src/utils/getValidationErrors.ts
+++ stage03/frontend-web/src/utils/getValidationErrors.ts
@@ -1,3 +1,15 @@
-export default function getValidationErrors(err) {
-  return {};
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
 }
