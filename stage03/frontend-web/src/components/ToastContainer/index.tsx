ad4f4591a005ac1b85f2cc0aadbf3bedf5f775e5
Bruno Fernandes <skyheads@live.com>
2020-05-19 19:49:13 -0300

    toast container component

--- stage03/frontend-web/src/components/ToastContainer/index.tsx
+++ stage03/frontend-web/src/components/ToastContainer/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+
+import { Container, Toast } from './styles';
 
 const ToastContainer: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Toast>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possível fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+    </Container>
+  );
 };
 
 export default ToastContainer;
