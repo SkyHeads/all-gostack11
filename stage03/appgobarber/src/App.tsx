3fd4e83e01c91c21a604f48269541fcadf24600d
Bruno Fernandes <skyheads@live.com>
2020-05-20 03:34:37 -0300

    install react navigation stack

--- stage03/appgobarber/src/App.tsx
+++ stage03/appgobarber/src/App.tsx
@@ -1,11 +1,15 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { StatusBar, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { StatusBar } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar backgroundColor="#312e38" barStyle="light-content" />
-    </>
+    </NavigationContainer>
   );
 };
 
