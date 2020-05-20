99db6e0ca1491cb772fd46eb4a81869463e8e39b
Bruno Fernandes <skyheads@live.com>
2020-05-20 03:17:58 -0300

    initial signin/signup pages

--- stage03/appgobarber/src/App.tsx
+++ stage03/appgobarber/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StatusBar, View } from 'react-native';
 
-import Title from './components/Title';
-
 const App: React.FC = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+    <>
       <StatusBar backgroundColor="#312e38" barStyle="light-content" />
-    </View>
+    </>
   );
 };
 
