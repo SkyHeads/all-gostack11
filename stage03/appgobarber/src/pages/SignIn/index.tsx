2654286b0e47faf7fede3d146caaf7027f6596c1
Bruno Fernandes <skyheads@live.com>
2020-05-20 03:21:38 -0300

    import container in pages

--- stage03/appgobarber/src/pages/SignIn/index.tsx
+++ stage03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Container } from './styles';
 
 const SignIn: React.FC = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  );
+  return <Container />;
 };
 
 export default SignIn;
--- stage03/appgobarber/src/pages/SignUp/index.tsx
+++ stage03/appgobarber/src/pages/SignUp/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Container } from './styles';
 
 const SignUp: React.FC = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
-  );
+  return <Container />;
 };
 
 export default SignUp;
