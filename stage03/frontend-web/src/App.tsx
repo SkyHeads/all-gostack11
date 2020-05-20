832cf92b918555905f239214622c77334646c0f7
Bruno Fernandes <skyheads@live.com>
2020-05-19 22:14:45 -0300

    import all providers in hooks/index

--- stage03/frontend-web/src/App.tsx
+++ stage03/frontend-web/src/App.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import ToastContainer from './components/ToastContainer';
-
-import { AuthProvider } from './hooks/auth';
-
 import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
+      </AppProvider>
 
-      <ToastContainer />
       <GlobalStyle />
     </>
   );
