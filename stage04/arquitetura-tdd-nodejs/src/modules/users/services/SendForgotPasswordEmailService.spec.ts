890510c35b81ae76370584cef6b2dc7739bd758d
Bruno Fernandes <skyheads@live.com>
2020-06-24 13:26:31 -0300

    test not be able recover non existing user email

--- stage04/arquitetura-tdd-nodejs/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ stage04/arquitetura-tdd-nodejs/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,5 +1,5 @@
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
-// import AppError from '../../../shared/errors/AppError';
+import AppError from '../../../shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
@@ -27,5 +27,21 @@ describe('SendForgotPasswordEmail', () => {
     });
 
     expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover a non-existing user email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await expect(
+      sendForgotPasswordEmail.execute({
+        email: 'johndoe@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
