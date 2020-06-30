2dd87e996a5099fca6ac5ec5c2cd7149ef353783
Bruno Fernandes <skyheads@live.com>
2020-06-30 00:55:37 -0300

    update profile service done and all tests

--- stage04/arquitetura-tdd-nodejs/src/modules/users/services/UpdateProfileService.ts
+++ stage04/arquitetura-tdd-nodejs/src/modules/users/services/UpdateProfileService.ts
@@ -1,8 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
+import User from '@modules/users/infra/typeorm/entities/User';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -24,7 +24,49 @@ class UpdateProfileService {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ name, email, user_id }: IRequest): Promise<void> {}
+  public async execute({
+    name,
+    email,
+    user_id,
+    old_password,
+    password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not found');
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError('E-mail already exists');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        'You need to inform the old password to set a new password.',
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not match');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return this.usersRepository.save(user);
+  }
 }
 
 export default UpdateProfileService;
