efd0caad2d50018bc7e44a3155ea13108ddee338
SkyHeads <skyheads@live.com>
2020-07-22 02:17:20 -0300

    show profile feature and adjusts

--- stage04/arquitetura-tdd-nodejs/src/modules/users/services/UpdateUserAvatarService.ts
+++ stage04/arquitetura-tdd-nodejs/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,12 +1,8 @@
-import { join } from 'path';
-import fs from 'fs';
-
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
 import User from '@modules/users/infra/typeorm/entities/User';
-import uploadConfig from '@config/upload';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
