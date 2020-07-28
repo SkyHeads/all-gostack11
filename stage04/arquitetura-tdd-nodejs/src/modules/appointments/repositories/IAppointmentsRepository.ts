df4ae1d2373082cf1bcc5da3fe5129287b9524b9
SkyHeads <skyheads@live.com>
2020-07-28 01:52:45 -0300

    filter appointments by month feature

--- stage04/arquitetura-tdd-nodejs/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ stage04/arquitetura-tdd-nodejs/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
+import IFindAllInMonthProviderDTO from '@modules/appointments/dtos/IFindAllInMonthProviderDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthProviderDTO,
+  ): Promise<Appointment[]>;
 }
