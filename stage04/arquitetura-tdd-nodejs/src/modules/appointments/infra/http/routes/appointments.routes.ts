d3b75e0b484aec81e2f8d5dd249021003597cae2
SkyHeads <skyheads@live.com>
2020-08-03 22:46:46 -0300

    install celebrate/import errors/valid appo routes

--- stage04/arquitetura-tdd-nodejs/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ stage04/arquitetura-tdd-nodejs/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
@@ -10,7 +10,16 @@ const providerController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
-appointmentRouter.post('/', appointmentsController.create);
+appointmentRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date().required(),
+    },
+  }),
+  appointmentsController.create,
+);
 
 appointmentRouter.get('/me', providerController.index);
 
