2e2488ece744467d3f41b860cf70dca8febed3a5
SkyHeads <skyheads@live.com>
2020-08-20 20:24:40 -0300

    install styled components

--- stage05/mobilegobarber/src/pages/Signup/index.tsx
+++ stage05/mobilegobarber/src/pages/Signup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const signup: React.FC = () => {
   return <View />;
